ece65560d75cb4cf5be294e2caaa514f04bc06e8
bayumss <bayu@mss.co.id>
2017-03-24 14:13:12 +0700

    updated: donwload page

--- components/download/download.controller.js
+++ components/download/download.controller.js
@@ -1,20 +1,13 @@
-app.controller('DownloadController', function($scope, $rootScope, $http, $location, oneDriveFactory) {
+app.controller('DownloadController', function($scope, $rootScope, $http, $location, fileService, $q) {
+    fileService.get().then(function(response) {
+        var promises = [];
+        angular.forEach(response.data, function(val) {
+            promises.push(val);
+            console.log(val)
+        });
 
-    // User search query 
-    $scope.query = $location.search().q;
-    // Search result 
-    $scope.items = null;
-    // Retrieve data from Office 365 
-    $scope.loadItems = function() {
-            oneDriveFactory.searchItems($scope.query)
-                .then(
-                    function(response) {
-                        // set result 
-                        $scope.items = response.data.value;
-                    }, $rootScope.responseError);
-        }
-        // if user query is not empty load data from Office 365 
-    if ($scope.query) {
-        $scope.loadItems();
-    }
+        $q.all(promises).then(function(response) {
+            $scope.files = response;
+        })
+    });
 });
