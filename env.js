8286b4f64b14503593771916196b9bfe6c843140
bayumss <bayu@mss.co.id>
2017-04-19 15:28:13 +0700

    merge pull

--- env.js
+++ env.js
@@ -2,5 +2,5 @@ var AUTH0_CLIENT_ID = '842g1mnOSHptfdW19KVlvyahlIzq8xwZ';
 var AUTH0_DOMAIN = 'mssid.auth0.com';
 var AUTH0_API_URL = 'https://mssid.auth0.com/api/v2';
 // var API = 'http://localhost:1234';
-var API='https://a.mss.co.id';
+var API = 'https://a.mss.co.id';
 var AUTH0_AUTH_URL = 'https://mssid.auth0.com';
